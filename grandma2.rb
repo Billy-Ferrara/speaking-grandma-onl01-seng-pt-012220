1258d364d14c8d189e7888ce96fec07e73c83175
Billy Ferrara <willfira717@gmail.com>
2020-01-27 01:54:52 +0000

    Automatically backed up by Learn

--- grandma2.rb
+++ grandma2.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
 
 if (phrase) == (phrase).upcase
-  print "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 elsif (phrase) == "I LOVE YOU GRANDMA!"
-print "I LOVE YOU TOO PUMPKIN!"
+return "I LOVE YOU TOO PUMPKIN!"
 else
-  print "HUGH?! SPEAK UP, SONNY!"
+  return "HUGH?! SPEAK UP, SONNY!"
 end
